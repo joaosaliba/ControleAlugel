217e789241f9df3a10b13507a362320aec3ec976
João G. Saliba <joao_salibaa@hotmail.com>
2020-12-30 22:38:43 -0300

    Arrumando base, finalizando CRUD IMOVEL,criando CRUD Pessoa

--- Aluguel/urls.py
+++ Aluguel/urls.py
@@ -4,8 +4,21 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new',views.createImovelView, name='create_imovel'),
+    path('create_imovel',views.createImovelView, name='create_imovel'),
     path('storeImovel',views.storeImovel, name= 'storeImovel'),
     path('listImoveis',views.list_imovel, name='listImoveis'),
     path('editImovel/<int:pk>',views.editImovel, name='editImovel'),
+    path('deleteImovel/<int:pk>',views.delete_imovel, name='deleteImovel'),
+
+    path('create_pessoa',views.create_pessoa, name='create_pessoa'),
+    path('list_pessoas',views.list_pessoa, name='list_pessoas'),
+    path('edit_pessoa/<int:pk>',views.edit_pessoa, name='edit_pessoa'),
+    path('delete_pessoa/<int:pk>',views.delete_pessoa, name='delete_pessoa'),
+
+
+
+
+
+
+
 ]
--- Aluguel/views.py
+++ Aluguel/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import ImovelForm
-from .models import Imovel
+from .models import Imovel, Pessoa
 from django.contrib import messages
 
 # Create your views here.
@@ -16,7 +16,7 @@ def index(request):
 #      return render(request, 'formImovel.html', {'form':form})
 
 def createImovelView(request):
-     return render(request,'createimovel.html')
+     return render(request,'imovel/createimovel.html')
 
 def storeImovel(request):
      imovel = Imovel()
@@ -31,7 +31,7 @@ def editImovel(request,pk):
      imovel= Imovel.objects.get(id=pk)
 
      if request.method =="GET":
-          return render(request,'editImovel.html',{'imovel':imovel})
+          return render(request,'imovel/editImovel.html',{'imovel':imovel})
      else:
           imovel.id = pk
           imovel.cep= request.POST.get('cep')
@@ -41,9 +41,50 @@ def editImovel(request,pk):
           return redirect('/listImoveis')
 
      
-
-     
-
 def list_imovel(request):
      imoveis = Imovel.objects.all()
-     return render(request, 'listImovel.html',{'imoveis':imoveis})
+     return render(request, 'imovel/listImovel.html',{'imoveis':imoveis})
+
+def delete_imovel(request,pk):
+     imovel = Imovel.objects.get(id=pk)
+     imovel.delete()
+     messages.success(request, "Imovel Deletado com Sucesso")
+     return redirect('/listImoveis')
+
+def create_pessoa(request):
+     pessoa = Pessoa()
+     if request.method =="GET":
+          return render(request,'pessoa/createPessoa.html',{'pessoa':pessoa})
+     else:
+          pessoa.nome= request.POST.get('nome')
+          pessoa.cpf= request.POST.get('cpf')
+          pessoa.rg= request.POST.get('rg')
+          pessoa.telefone= request.POST.get('telefone')
+          pessoa.save()
+          messages.success(request,'Pessoa adiconado com sucesso')
+          return redirect('/list_pessoas')
+
+def list_pessoa(request):
+     pessoas = Pessoa.objects.all()
+     return render(request, 'pessoa/listPessoas.html',{'pessoas':pessoas})
+
+def edit_pessoa(request,pk):
+     pessoa = Pessoa.objects.get(id=pk)
+
+     if request.method =="GET":
+          return render(request,'pessoa/editPessoa.html',{'pessoa':pessoa})
+     else:
+          pessoa.id =pk
+          pessoa.nome= request.POST.get('nome')
+          pessoa.cpf= request.POST.get('cpf')
+          pessoa.rg= request.POST.get('rg')
+          pessoa.telefone= request.POST.get('telefone')
+          pessoa.save()
+          messages.success(request,'Pessoa adiconado com sucesso')
+          return redirect('/list_pessoas')
+
+def delete_pessoa(request,pk):
+     pessoa = Pessoa.objects.get(id=pk)
+     pessoa.delete()
+     messages.success(request, "Pessoa deletada com Sucesso")
+     return redirect('/list_pessoas')
